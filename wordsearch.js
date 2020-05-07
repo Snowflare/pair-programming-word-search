55253dbd3bb358f9ed8cfb9c68ff404763cddf2a
Daniel Zhang <danielzhang@Daniels-MacBook-Pro.local>
2020-05-06 18:59:19 -0700

    wordsearch.js improved

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
+const transpose = require('./transpose');
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
+        if (l.includes(reverseStr(word))) return true;
+
     }
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    for (l of verticalJoin) {
+        if (l.includes(word)) return true;
+        if (l.includes(reverseStr(word))) return true;
+    }
+    return false;
+
+    
+}
+
+
+const reverseStr = (str) => {
+    return str.split('').reverse().join('');
 }
 
 module.exports = wordSearch
